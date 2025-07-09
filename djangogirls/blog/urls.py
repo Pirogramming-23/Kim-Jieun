7bbd6f472f06c3fb51070158bbe81d64e3129aea
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-09 15:38:03 +0900

    djangogirls - 애플리케이션 확장하기 학습완료

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name = 'post_list'), 
     #views.post_list를 보여주도록 함. name은 URL에 붙인 이름
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # url이 post 문자를 포함해야 함/int 형으로 기대되는 pk 변수를 뷰로 전송
 ]
