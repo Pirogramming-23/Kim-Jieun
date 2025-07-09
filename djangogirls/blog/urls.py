051548303908e6ae7582e5b39916d5c815749ed4
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-09 16:01:54 +0900

    djangogirls - Django 폼 학습완료

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     #views.post_list를 보여주도록 함. name은 URL에 붙인 이름
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     # url이 post 문자를 포함해야 함/int 형으로 기대되는 pk 변수를 뷰로 전송
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
