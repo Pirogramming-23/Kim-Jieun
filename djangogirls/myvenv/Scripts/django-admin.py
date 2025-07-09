48e57389806133d724063e4801adf9a1a3eb40c7
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-09 10:57:59 +0900

    djangogirls - 나의 첫 번째 Django 프로젝트! 학습완료

--- djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\진짐\Desktop\piro git 실습\Kim-Jieun\djangogirls\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
