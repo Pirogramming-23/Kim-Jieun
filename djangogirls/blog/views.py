302e986a112a1028a3ef6159ec9ab46af8e49f29
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-09 12:24:48 +0900

    djangogirls - 템플릿 동적 데이터 학습완료

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # view : app의 로직을 넣는 곳
 def post_list(request):
+    posts = Post.objects.filter(published_dae__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
