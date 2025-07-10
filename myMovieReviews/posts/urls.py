ddee4c15488021678bb8b1f33de8f379f333faa6
j2nii <rlawldmsjieuni0218@gmail.com>
2025-07-10 19:45:55 +0900

    review 상세보기 기능 추가

--- myMovieReviews/posts/urls.py
+++ myMovieReviews/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path("", reviews_list),
+    path("posts/<int:pk>/", reviews_read),
 ]
--- myMovieReviews/posts/views.py
+++ myMovieReviews/posts/views.py
@@ -6,3 +6,8 @@ def reviews_list(request):
     reviews = Review.objects.all()
     context = {"reviews" : reviews}
     return render(request, "reviews_list.html", context)
+
+def reviews_read(request, pk):
+    review = Review.objects.get(id=pk)
+    context = {"review" : review}
+    return render(request, "reviews_read.html", context)
